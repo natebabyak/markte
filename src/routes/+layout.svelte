4a161ea3c441d9f1795b0e13ec729dbbfabd2adf
Nate Babyak <nate.babyak@outlook.com>
2025-06-24 14:33:25 -0400

    fix github-markdown.css

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,24 +2,22 @@
 	import '../app.css';
 	import { mode, ModeWatcher } from 'mode-watcher';
 	import * as Tooltip from '$lib/components/ui/tooltip';
+	import darkCss from 'github-markdown-css/github-markdown-dark.css?url';
+	import lightCss from 'github-markdown-css/github-markdown-light.css?url';
 
 	let { children } = $props();
 	let link: HTMLLinkElement;
 
 	$effect(() => {
-		if (link) {
-			link.remove();
-		}
+		if (link) link.remove();
 
 		link = document.createElement('link');
 		link.rel = 'stylesheet';
-		link.href = `/node_modules/github-markdown-css/github-markdown-${mode.current}.css`;
+		link.href = mode.current === 'dark' ? darkCss : lightCss;
 		document.head.appendChild(link);
 
 		return () => {
-			if (link) {
-				link.remove();
-			}
+			if (link) link.remove();
 		};
 	});
 </script>
